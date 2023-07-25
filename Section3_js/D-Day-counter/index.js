10e191f90eb8dd3ae879a5168614b1214a4eaef3
Jinstarship-code <yyjh6714@hanmail.net>
2023-07-25 21:35:17 +0900

    07_25_js_storage

--- Section3_js/D-Day-counter/index.js
+++ Section3_js/D-Day-counter/index.js
@@ -1,3 +1,8 @@
+const messageContainer = document.querySelector("#d-day-message");
+const container = document.querySelector("#d-day-container");
+
+messageContainer.innerHTML = "<h3>D-Day를 입력해주세요.</h3>";
+
 const dataFromMaker = () => {
   const inputYear = document.querySelector("#target-year-input").value;
   const inputMonth = document.querySelector("#target-month-input").value;
@@ -7,8 +12,6 @@ const dataFromMaker = () => {
 };
 
 const counterMaker = () => {
-  const messageContainer = document.querySelector("#d-day-message");
-  messageContainer.textContent = "D-Day를 입력해 주세요.";
   const targetDateInput = dataFromMaker();
   const nowDate = new Date();
   const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0);
@@ -16,13 +19,48 @@ const counterMaker = () => {
 
   if (remaining <= 0) {
     console.log("타이머가 종료되었습니다.");
+    messageContainer.innerHTML = "<h3>타이머가 종료되었습니다.</h3>";
   } else if (isNaN(remaining)) {
     console.log("잘못 입력하셨습니다.");
+    messageContainer.innerHTML = "<h3>유효한 시간대가 아닙니다..</h3>";
   }
 
-  const remainingDate = Math.floor(remaining / 3600 / 24);
-  const remainingHours = Math.floor(remaining / 3600) % 24;
-  const remainingMin = Math.floor(remaining / 60) % 60;
-  const remainingSec = Math.floor(remaining) % 60;
-  console.log(remainingDate, remainingHours, remainingMin, remainingSec);
+  // const remainingDate = Math.floor(remaining / 3600 / 24);
+  // const remainingHours = Math.floor(remaining / 3600) % 24;
+  // const remainingMin = Math.floor(remaining / 60) % 60;
+  // const remainingSec = Math.floor(remaining) % 60;
+
+  const remainingObj = {
+    remainingDate: Math.floor(remaining / 3600 / 24),
+    remainingHours: Math.floor(remaining / 3600) % 24,
+    remainingMin: Math.floor(remaining / 60) % 60,
+    remainingSec: Math.floor(remaining) % 60,
+  };
+
+  const documentObj = {
+    days: document.getElementById("days"),
+    hours: document.getElementById("hours"),
+    min: document.getElementById("min"),
+    sec: document.getElementById("sec"),
+  };
+
+  const timeKeys = Object.keys(remainingObj);
+  const documentArr = ["days", "hours", "min", "sec"];
+
+  i = 0;
+  for (let tag of documentArr) {
+    document.getElementById(tag).textContent = remainingObj[timeKeys[i]];
+    i++;
+  }
+
+  // documentObj.days.textContent = remainingObj.remainingDate;
+  // documentObj.hours.textContent = remainingObj.remainingHours;
+  // documentObj.min.textContent = remainingObj.remainingMin;
+  // documentObj.sec.textContent = remainingObj.remainingSec;
+};
+
+const starter = function () {
+  container.style.display = "flex";
+  messageContainer.style.display = "none";
+  counterMaker();
 };
